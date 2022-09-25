6726860e258a5bd511894122981466e1e65c208f
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2022-09-25 21:23:38 +0100

    fixed mic capture playback

--- src/modules/senses/sound.py
+++ src/modules/senses/sound.py
@@ -1,15 +1,33 @@
 import speech_recognition as sr 
 import pyttsx3
 import pyaudio
-from simhash import Simhash
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 # voices = engine.getProperty('voices')
 # engine.setProperty('voice', voices[1].id)
 
-print(Simhash('hello').value)
-print(Simhash('hello1').value)
+
+def play(audio):
+    '''
+    Play audio captured from microphone
+    '''
+    CHUNK = 1024
+    
+    p = pyaudio.PyAudio()
+    stream = p.open(
+        format=p.get_format_from_width(audio.sample_width),
+        channels=1,
+        rate=audio.sample_rate,
+        output=True
+    )
+    
+    wav = audio.get_wav_data()
+    
+    stream.write(wav)
+    stream.close()
+    
+    p.terminate()
 
 def talk(text:str):
     engine.say(text)
@@ -17,15 +35,16 @@ def talk(text:str):
 
 def listen_on_microphone():
     try:
-        with sr.Microphone(device_index=1) as source:
-            print('Listening...')
-            listener.adjust_for_ambient_noise(source)
-            voice = listener.listen(source=source)
-            print('Now checking on google')
-            command = listener.recognize_google(voice)
-            print(command)
-            if 'argon' in command:
-                talk(command)
+        while True:
+            with sr.Microphone(device_index=1) as source:
+                print('Listening...')
+                listener.adjust_for_ambient_noise(source)
+                voice = listener.listen(source=source)
+                print('Now checking on google')
+                command = listener.recognize_google(voice)
+                print(command)
+                if 'argon' in command:
+                    talk(command)
     except Exception as e:
         print(str(e))
 
